bacbe72d3cdf47dbd48c83edb734b402ee1a5c2f
SSunH <tjsgh1104@naver.com>
2023-06-07 03:30:13 +0900

    스프링부트에 react 추가하기

---
+++ react001/src/main/java/com/example/react001/React001Application.java
@@ -0,0 +1,13 @@
+package com.example.react001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class React001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(React001Application.class, args);
+	}
+
+}
---
+++ react001/src/test/java/com/example/react001/React001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.react001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class React001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
